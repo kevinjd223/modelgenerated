a96f4490d3f50e3a9a6509b9965f37765a9667b2
kevinjd223 <kevinjd223@gmail.com>
2025-12-27 13:23:06 -0800

    MG-040 update to EJB 4

--- src/main/java/com/modelgenerated/authentication/AuthenticationServiceBean.java
+++ src/main/java/com/modelgenerated/authentication/AuthenticationServiceBean.java
@@ -7,8 +7,6 @@
 package com.modelgenerated.authentication;
 
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
 
 import com.modelgenerated.authentication.dao.AccountDAO;
 import com.modelgenerated.authentication.dao.TenantDAO;
@@ -22,6 +20,11 @@ import com.modelgenerated.foundation.identity.Identity;
 import com.modelgenerated.foundation.identity.IdentityBuilder;
 import com.modelgenerated.foundation.logging.Logger;
 
+import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
+import jakarta.ejb.TransactionAttribute;
+import jakarta.ejb.TransactionAttributeType;
+
 /**
  *
  * @author  kevind
@@ -29,12 +32,13 @@ import com.modelgenerated.foundation.logging.Logger;
 @Stateless
 @EJB(name = "java:global/AuthenticationService", beanInterface = AuthenticationService.class)
 public class AuthenticationServiceBean implements AuthenticationService {
-	
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
     public UserContext Authenticate(String userID, String password, String tenantName) {
     	return Authenticate(userID, password, tenantName, 5);
     }
-    
+
     private UserContext Authenticate(String userName, String password, String tenantName, int badAttemptLimit) {
         AccountDAO accountDAO = (AccountDAO)DataAccessLocator.findDAO(Account.class.getName());
         TenantDAO tenantDAO = (TenantDAO)DataAccessLocator.findDAO(Tenant.class.getName());
@@ -78,7 +82,8 @@ public class AuthenticationServiceBean implements AuthenticationService {
         }
         return null;        
     }
-    
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
     public Account newAccount(UserContext userContext) {
         Account account = (Account)Factory.createObject(Account.class.getName());
@@ -86,7 +91,8 @@ public class AuthenticationServiceBean implements AuthenticationService {
         account.setId(id);
         return account;
     }
-    
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     @Override
     public int saveAccount(UserContext userContext, Account account) {
     	int retValue = AuthenticationService.SUCCESS;
@@ -108,7 +114,8 @@ public class AuthenticationServiceBean implements AuthenticationService {
         
         return retValue;
     }
-    
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
     public AccountList getAccounts(UserContext userContext) {
         AccountDAO accountDAO = (AccountDAO)DataAccessLocator.findDAO(Account.class.getName());
@@ -117,7 +124,8 @@ public class AuthenticationServiceBean implements AuthenticationService {
         AccountList accountList = accountDAO.search(userContext, searchCriteria, false);
         return accountList;        
     }
-    
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
     public Account findAccount(UserContext userContext, String id) {
         AccountDAO accountDAO = (AccountDAO)DataAccessLocator.findDAO(Account.class.getName());
@@ -128,6 +136,7 @@ public class AuthenticationServiceBean implements AuthenticationService {
         return account;        
     }
 
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
     public Account findAccountByUserName(UserContext userContext, String userName) {
         AccountDAO accountDAO = (AccountDAO)DataAccessLocator.findDAO(Account.class.getName());
@@ -135,7 +144,8 @@ public class AuthenticationServiceBean implements AuthenticationService {
         Account account = (Account)accountDAO.findByUserName(userContext, userName);
         return account;        
     }
-    
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
     public Tenant newTenant(UserContext userContext) {
     	Tenant tenant = (Tenant)Factory.createObject(Tenant.class.getName());
@@ -143,7 +153,8 @@ public class AuthenticationServiceBean implements AuthenticationService {
 	    tenant.setId(id);
 	    return tenant;
 	}
-    
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     @Override
     public int saveTenant(UserContext userContext, Tenant tenant) {
 		int retValue = AuthenticationService.SUCCESS;
@@ -161,7 +172,8 @@ public class AuthenticationServiceBean implements AuthenticationService {
 	    
 	    return retValue;
 	}
-    
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
     public Tenant findTenant(UserContext userContext, String id) {
     	TenantDAO tenantDAO = (TenantDAO)DataAccessLocator.findDAO(Tenant.class.getName());
@@ -171,7 +183,8 @@ public class AuthenticationServiceBean implements AuthenticationService {
 	    Logger.debug(this, "findTenantid=" + id);
 	    return tenant;        
 	}
-    
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     @Override
     public TenantList getTenants(UserContext userContext) {
     	TenantDAO tenantDAO = (TenantDAO)DataAccessLocator.findDAO(Tenant.class.getName());
